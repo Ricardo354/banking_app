03d6a2260cd85ae04bac448d81bdb7d3b0c8b0d9
Ricardo354 <yellow_richard@proton.me>
2025-03-29 16:35:30 -0300

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="translate-x-[145%] flex items-center justify-center min-h-screen text-white">
+      <h1 className="text-4xl font-bold">Hello, Tailwind with Vite! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
