bc468a30c0cb061dd35aa23265db5e52a2979f30
Ricardo354 <yellow_richard@proton.me>
2025-03-30 00:44:33 -0300

    Adicionando flechazinha q aparece qnd vc seleciona uma pagina

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import walletIcon from "../assets/wallet.png";
-import { HomeIcon, TransactionIcon, creditCardIcon, paymentsIcon} from "./Icons";
+import { HomeIcon, TransactionIcon, creditCardIcon, paymentsIcon, recipientIcon, settingsIcon } from "./Icons";
 
 // ajeitar o ngç do stroke dinamismo
 
@@ -8,8 +8,10 @@ const menuItems = [
 
     { Nome: "Home", Icon: HomeIcon },
     { Nome: "Transactions", Icon: TransactionIcon },
-    { Nome: "Cards" , Icon: creditCardIcon},
-    { Nome:  "Payments", Icon: paymentsIcon}
+    { Nome: "Cards", Icon: creditCardIcon },
+    { Nome: "Payments", Icon: paymentsIcon },
+    { Nome: "Recipients", Icon: recipientIcon },
+    { Nome: "Settings", Icon: settingsIcon }
 ]
 
 const Navbar = () => {
@@ -19,28 +21,33 @@ const Navbar = () => {
     return (
         <>
             <div className="flex flex-col items-center bg-zinc-900 min-h-screen max-w-[15%] gap-x-3 border-r-2 border-zinc-800">
-                <div className="mt-3 gap-x-1 flex flex-row">
+                <div className="mt-3 gap-x-1 flex flex-row mr-6">
                     <img src={walletIcon} alt="wallet_icon" className="w-12 h-12 my-5" />
                     <h1 className="py-[19%] text-blue-500 font-fredoka text-2xl">
                         R1 Banking
                     </h1>
                 </div>
-
-                <div className="gap-y-4 flex flex-col mt-[23%]">
+                <div className="gap-y-6 flex flex-col mt-[23%]">
                     {menuItems.map((item) => (
-                        <div className="cursor-pointer flex mr-20 ml-10">
-                            <item.Icon className={`w-8 h-14 stroke-[1.5]${selected === item.Nome ? "fill-white stroke-white" : "fill-zinc-600 stroke-zinc-600 "}`} />
-                            <h1 className={`ml-2 mt-4 text-xl ${selected === item.Nome ? "text-white" : "text-zinc-600"}`} onClick={() => setSelected(item.Nome)}>
-                                {item.Nome}
-                            </h1>
-                        </div>
+                        <>
+                            <div className="flex flex-row cursor-pointer flex mr-20 ml-10">
+                                <item.Icon className={`w-8 h-14 ${selected === item.Nome ? "fill-white" : "fill-zinc-600"}`} />
+                                <h1 className={`ml-2 mt-4 text-xl ${selected === item.Nome ? "text-white" : "text-zinc-600"}`} onClick={() => setSelected(item.Nome)}>
+                                    {item.Nome}
+                                </h1>
+                                {selected === item.Nome && (
+                                    <svg className="left-0 absolute -translate-x-[55%] fill-blue-600" viewBox="0 40 60 50" width={55} height={55}>
+                                        <rect rx="10" ry="10" x="50" y="5" width="40" height="40" transform="rotate(45)" />
+                                    </svg>
+                                )}
+
+                            </div>
+                        </>
                     ))}
-
+                    <div>
+                    </div>
                 </div>
-
             </div>
-
-
         </>
     )
 }
