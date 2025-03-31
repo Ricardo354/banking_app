cf23b68318e119427d3694db9e08e5fcd7b36375
Ricardo354 <yellow_richard@proton.me>
2025-03-31 00:06:08 -0300

    Finishing main layouof recent transactions and changing fonts in entire app

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -67,10 +67,17 @@ export const LtIcon = (props) => (
 );
 
 export const GtIcon = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 24 24" 
-    className={`${props.className}`}>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className={`${props.className}`}>
         <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+    </svg>
+
+);
+
+export const AddIcon = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}    className={`${props.className}`}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
 
 );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import Navbar from "../components/Navbar";
-import { LtIcon, GtIcon } from "../components/Icons";
+import { LtIcon, GtIcon, AddIcon } from "../components/Icons";
 
+
+// add brilho no Add Card
+// add brilho nas setinha   
 const Home = () => {
+
+    const mockTransaction = [
+        { Description: "Guitarra", Category: "Transfer", Price: 2000.00 },
+        { Description: "Sanduiche pra Cecilia", Category: "Transfer", Price: 30.00 },
+        { Description: "Uber", Category: "Transfer", Price: 30.00 },
+        { Description: "Sorvete pra Cecilia", Category: "Transfer", Price: 16.00 },
+    ]
+
     return (
         <>
             <div className="h-screen flex flex-row bg-zinc-800 bg-zinc-950">
                 <Navbar />
                 <div className="flex flex-col h-screen w-screen">
                     <div className="mt-20 ml-14 rounded-3xl bg-card-black w-[40%] h-[45%]">
-                        <h1 className="ml-3 font-oswald text-white flex flex-row items-center text-3xl p-8">
+                        <h1 className="w-[100%] ml-3 font-oswald text-white flex flex-row items-center text-3xl p-8">
                             My card
                             <button className="mt-[1%] bg-zinc-950 rounded-full w-7 ml-3 h-7">
                                 <LtIcon className="w-6 fill-neutral-600" />
@@ -16,7 +27,18 @@ const Home = () => {
                             <button className="mt-[1%] bg-zinc-950 rounded-full w-7 ml-2  h-7">
                                 <GtIcon className="w-6 fill-neutral-600 ml-[10%]" />
                             </button>
+
+
+                            <div className="ml-[50%] flex items-center cursor-pointer">
+                                <button className="mt-1 bg-zinc-950 rounded-full w-8 h-8 flex items-center justify-center">
+                                    <AddIcon className="w-6 stroke-neutral-600" />
+                                </button>
+                                <h1 className="font-raleway mt-1 ml-1 text-xl text-blue-500">
+                                    Add Card
+                                </h1>
+                            </div>
                         </h1>
+
 
                         <div className="flex flex-row  mr-1 m-8 mt-0 mb-0 h-[69%]">
                             <div className="flex justify-center items-center p-2 rounded-3xl w-[90%] bg-gradient-to-r from-sky-800 via-sky-900 to-sky-950 text-white">
@@ -32,7 +54,35 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="border border-white mt-5 ml-14 rounded-3xl w-[40%] h-[46%]"></div>
+                    <div className="mt-5 ml-14 rounded-3xl w-[40%] h-[46%]">
+                        <h1 className="ml-3 font-oswald text-white flex flex-row items-center text-3xl p-8">
+                            Recent Transactions
+                            <button className="text-blue-500 font-raleway text-2xl ml-[49%]">
+                                See All
+                            </button>
+                        </h1>
+
+                        <div className="flex flex-col gap-y-6">
+                            {mockTransaction.map((transaction) =>
+
+                                <div className="flex justify-between text-white w-[90%] h-14 ml-[7%]">
+                                    <div className="border border-zinc-900 rounded-xl w-[10%] h-30">
+                                    </div>
+
+                                    <h1 className="flex-1 font-raleway -mt-1 ml-2 p-2 text-xl truncate">
+                                        {transaction.Description}
+                                        <p className="ml-[1%] text-sm">{transaction.Category}</p>
+                                    </h1>
+                                    
+                                    <h1 className="font-oswald ml-[30%] mr-3  text-md text-white">
+                                        {transaction.Category === "Transfer" ? "-" : "+"} R$ {transaction.Price.toFixed(2)}
+                                    </h1>
+
+                                </div>
+                            )}
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
